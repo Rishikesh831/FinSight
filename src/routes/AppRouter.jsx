2c5e82e45bafc89debcd89d9bc4c7a39e65be417
unknown <rishikesh.bhatt24@spit.ac.in>
2025-11-01 11:23:20 +0530

    service setup

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,10 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
 import LandingPage from "../pages/LandingPage";
+import DashboardPage from "../pages/DashboardPage";
+import InvestmentPage from "../pages/InvestmentPage";
+import DetailsPage from "../pages/DetailsPage";
+import QuadrantPage from "../pages/QuadrantPage";
 
 export default function AppRouter() {
     return (
@@ -9,6 +13,10 @@ export default function AppRouter() {
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
                 {/* More routes will be added later */}
+                <Route path="/dashboard" element={<DashboardPage/>}/>
+                <Route path="/investments" element={<InvestmentPage/>}/>
+                <Route path="/details" element={<DetailsPage/>}/>
+                <Route path="/quadrant" element={<QuadrantPage/>}/>
             </Routes>
         </BrowserRouter>
 
